19ad7d5b460ec0708afb8650a72019cd62129a24
Muhammad Franklin Yusuf <yusuffranklin@gmail.com>
2023-08-07 11:50:01 +0700

    Update outputs.tf
    add 'sensitive = true' in kube_config

--- configuration/iaac/azure/kubernetes/outputs.tf
+++ configuration/iaac/azure/kubernetes/outputs.tf
@@ -12,6 +12,7 @@ output "cluster_ca_certificate" {
 
 output "cluster_username" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.username
+  sensitive = true
 }
 
 output "cluster_password" {
